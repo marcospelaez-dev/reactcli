7fabef1663243e17fff5ea3d8ddb438fed6b73f6
Marcos Sousa <pelaez.marcos@gmail.com>
2024-01-29 16:45:23 -0300

    Inserindo Rotas
    Install react-router-dom e configurando as rotas no nosso projeto.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 
 function App() {
   return (
-    <div className="App">
-      Olá DIO
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
